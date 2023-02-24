843a4cebae7473d4131fa5c28c27d70f3d31ef61
WebSnke <websnke@tutanota.com>
2023-02-24 16:12:45 +0100

    Replace import with fetch() to import JSON data

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
 import { createMarkerAndPopup } from "./map.js";
-import museums from '../museums.json' assert {type: 'json'};
+
+fetch('https://raw.githubusercontent.com/WebSnke/The-Stolen-Map/main/museums.json')
+    .then(response => response.json())
+    .then(museums => {
+        for (let museum of museums) {
+            createMarkerAndPopup(museum);
+
+            for (let artifact of museum.artifacts) {
+                createMarkerAndPopup(artifact);
+            }
+        }
+    });
 
 for (let museum of museums) {
     createMarkerAndPopup(museum);
